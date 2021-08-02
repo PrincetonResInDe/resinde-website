7cbc93446556caf69a7593f25a17ff6bf4cec4c9
is-1 <65904475+is-1@users.noreply.github.com>
2021-08-03 07:44:56 +0900

    Update thanks.js

--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -1,21 +1,48 @@
 import React from "react"
 import tw, { styled } from "twin.macro"
 import { Link } from "gatsby"
+import Img from "gatsby-image"
+import { useStaticQuery, graphql } from "gatsby"
 
 const StyledLink = styled(props => <Link {...props} />)`
   ${tw`text-blue`}
+`
+
+const MagentaBlob = styled(Img)`
+  left: -12em;
+  top: 8em;
+  z-index: -1;
+  @media (max-width: 639px) {
+    left: -60px;
+  }
+`
+const Header1 = styled.h1`
+  ${tw`text-white text-7xl absolute pl-48`}
+  top: 300px;
 `
 
 const HeroContainer = tw.div`
   flex flex-col min-h-screen max-w-max justify-center mobile:px-8 px-48
 `
 
-const NotFoundPage = () => (
-  <HeroContainer>
-    <h1>Thank you!</h1>
-    <p>We'll get back to you soon.</p>
-    <StyledLink to="/">Back to the homepage.</StyledLink>
+const NotFoundPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      pinkBlob: file(relativePath: { eq: "pink-blob.png" }) {
+        childImageSharp {
+          fixed(width: 1025) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `)
+  return (
+  <HeroContainer >
+    <MagentaBlob fixed={data.pinkBlob.childImageSharp.fixed} style={{position: "relative", width: "1025px"}}/>
+    <Header1><br></br>Your inquiry <br></br><br></br> has been <br></br><br></br>received!</Header1>
   </HeroContainer>
-)
+  )
+}
 
 export default NotFoundPage
