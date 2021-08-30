3595ec65bce0c29315aab08e4a37fd6c681f15ec
Kyra Acquah <20865277+kyracq@users.noreply.github.com>
2021-08-30 16:01:33 -0400

    add grey logo

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,34 +1,66 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 import tw, { styled } from "twin.macro"
-import Seo from "../components/seo"
 import Button from "../components/button"
+import Img from "gatsby-image"
 
 const StyledP = styled.p`
   ${tw`text-center`}
 `
-
-const HeroContainer = tw.div`
+const Wrapper = tw.div`
   flex flex-col min-h-screen max-w-max justify-center mobile:px-8 px-48
 `
 
-const NotFoundPage = () => (
-  <HeroContainer>
-    <Seo title="404: Not found" />
-    <h1>WHOOPS</h1>
-    <br></br>
-    <StyledP>It seems like we've encountered an error!</StyledP>
-    <br></br>
-    <StyledP>
-      We apologize for that!
-      <br></br>
-      <br></br>
-      <br></br>
-      <Button to="/" blue="true">
-        GO HOME
-      </Button>
-    </StyledP>
-    <br></br>
-  </HeroContainer>
-)
+const TextContainer = tw.div`
+  flex flex-col self-center
+`
+
+const StyledDiv = tw.div`
+  flex space-x-8
+`
+
+const NotFoundPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      file: file(relativePath: { eq: "grey-resinde-logo.png" }) {
+        childImageSharp {
+          # Specify the image processing specifications right in the query.
+          fixed(width: 400, height: 492) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <Wrapper>
+      <StyledDiv>
+        <div>
+          <Img
+            fixed={data.file.childImageSharp.fixed}
+            alt="grey ResInDe logo"
+          />
+        </div>
+        <TextContainer>
+          <h1>WHOOPS</h1>
+          <br></br>
+          <StyledP>It seems like we've encountered an error!</StyledP>
+          <br></br>
+          <StyledP>
+            We apologize for that!
+            <br></br>
+            <br></br>
+            <br></br>
+            <Button to="/" blue="true">
+              GO HOME
+            </Button>
+          </StyledP>
+          <br></br>
+        </TextContainer>
+      </StyledDiv>
+    </Wrapper>
+  )
+}
 
 export default NotFoundPage
