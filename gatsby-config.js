00c0023d54ba9961a627c6f798cf04cc830444ff
thanyabegum <thanyab15@gmail.com>
2021-12-09 01:01:28 -0500

    Update projects, blobs

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,7 +40,7 @@ module.exports = {
         background_color: `#fff`,
         theme_color: `#fff`,
         display: `minimal-ui`,
-        icon: `src/images/resinde-logo-square.png`, // This path is relative to the root of the site.
+        icon: `src/images/logos/resinde-logo-square.png`, // This path is relative to the root of the site.
       },
     },
     {
